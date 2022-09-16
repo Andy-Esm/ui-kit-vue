bce6aa9423d0af49316bcdd037b662bdc23abbb7
Andrey Esman <andreyesman.md@gmil.com>
2022-09-16 10:31:28 +0300

    Переписан компонент кнопки

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,71 +1,68 @@
 <template>
-	<div class="inner-grid btn-wrapper">
-		<button class="btn--sm btn-prime">{{ label }}</button>
-		<button class="btn--md btn-prime">{{ label }}</button>
-		<button class="btn--lg btn-prime">{{ label }}</button>
-		<button class="btn--sm btn-outline">{{ label }}</button>
-		<button class="btn--lg btn-outline">{{ label }}</button>
-		<button class="btn--md btn-outline">{{ label }}</button>
-		<button class="btn--sm btn-hover">{{ label }}</button>
-		<button class="btn--md btn-hover">{{ label }}</button>
-		<button class="btn--lg btn-hover">{{ label }}</button>
-		<button class="btn--sm btn-disabled">{{ label }}</button>
-		<button class="btn--md btn-disabled">{{ label }}</button>
-		<button class="btn--lg btn-disabled">{{ label }}</button>
-	</div>
+  <div>
+    <button :class="['btn', `btn--${size}`, `btn--${color}`]">
+      {{ label }}
+    </button>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-	label: {
-		type: String,
-		default: 'Button',
-	},
+  label: {
+    type: String,
+    default: "Button",
+  },
+  size: {
+    type: String,
+    default: "sm",
+  },
+  color: {
+    type: String,
+    default: "prime",
+  },
 });
 </script>
 
 <style lang="scss" scoped>
-.btn-wrapper {
-	max-width: 30em;
+.btn {
+  display: grid;
+  align-items: center;
+  border-radius: 8px;
+  background-color: var(--btn-clr);
+  color: var(--light-clr);
+  &--prime{
+    background-color: var(--btn-clr);
+  }
+  &--outline{
+    background-color: transparent;
+    border: 1px solid var(--primary-clr);
+    color: var(--primary-clr);
+  }
+  &--hover{
+    background-color: var(--btn-hover);
+    border: 1px solid var(--primary-clr);
+  }
+  &--disabled{
+    background-color: var(--btn-disable);
+    cursor: auto;
+  }
+  &--sm {
+    padding: 8px 40px;
+    font-size: var(--fs-btn-reg);
+    font-weight: var(--fw-btn-sm);
+  }
+  &--md {
+    padding: 14px 40px;
+    font-size: var(--fs-btn-reg);
+    font-weight: var(--fw-btn-reg);
+  }
+  &--lg {
+    padding: 17px 55px;
+    font-size: var(--fs-btn-reg);
+    font-weight: var(--fw-btn-lg);
+  }
+
+
 }
 
-.btn--sm {
-	padding: 8px 40px;
-	font-size: var(--fs-btn-reg);
-	font-weight: var(--fw-btn-sm);
-	color: var(--light-clr);
-	line-height: 32px;
-	border-radius: 8px;
-}
-.btn--md {
-	padding: 14px 40px;
-	font-size: var(--fs-btn-reg);
-	font-weight: var(--fw-btn-reg);
-	color: var(--light-clr);
-	line-height: 32px;
-	border-radius: 8px;
-}
-.btn--lg {
-	padding: 17px 55px;
-	font-size: var(--fs-btn-reg);
-	font-weight: var(--fw-btn-lg);
-	color: var(--light-clr);
-	line-height: 32px;
-	border-radius: 8px;
-}
-.btn-prime {
-	background-color: var(--btn-clr);
-	color: var();
-}
-.btn-outline {
-	border: 1px solid var(--primary-clr);
-	color: var(--primary-clr);
-}
-.btn-hover {
-	background-color: var(--btn-hover);
-	border: 1px solid var(--primary-clr);
-}
-.btn-disabled {
-	background-color: var(--btn-disable);
-}
 </style>
--- src/views/Buttons.vue
+++ src/views/Buttons.vue
@@ -1,12 +1,40 @@
 <template>
-	<div class="container">
-		<h1 class="heading-h1">Buttons</h1>
-		<Button label="Primary" />
-	</div>
+  <div class="container">
+    <h1 class="heading-h1">Buttons</h1>
+    <div class="inner-grid">
+      <div class="btn-wrapper">
+        <h4 class="heading-h4">Small</h4>
+        <Button label="Primary" color="prime" />
+        <Button label="Primary" color="outline" />
+        <Button label="Primary" color="hover" />
+        <Button label="Primary" color="disabled" />
+      </div>
+      <div class="btn-wrapper">
+        <h4 class="heading-h4">Medium</h4>
+        <Button label="Primary" color="prime" size="md" />
+        <Button label="Primary" color="outline" size="md"/>
+        <Button label="Primary" color="hover" size="md"/>
+        <Button label="Primary" color="disabled" size="md"/>
+      </div>
+      <div class="btn-wrapper">
+        <h4 class="heading-h4">Large</h4>
+        <Button label="Primary" color="prime" size="lg" />
+        <Button label="Primary" color="outline" size="lg" />
+        <Button label="Primary" color="hover" size="lg" />
+        <Button label="Primary" color="disabled" size="lg"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import Button from '@/components/Button.vue';
+import Button from "@/components/Button.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+</style>
